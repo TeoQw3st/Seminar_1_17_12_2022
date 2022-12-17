e9975a837833cf9376f0751b03bb97f725b0a387
Pavel <Qw3st@mail.ru>
2022-12-17 21:43:05 +0300

    DZ_06

---
+++ DZ/DZ_06/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Это программа в идеале будет проверять является число четным или нет");
+Console.WriteLine("Введие число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number % 2 == 0)
+    {
+        Console.WriteLine($"Число {number} четное");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не четное");
+            }
+
