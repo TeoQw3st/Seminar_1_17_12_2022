13535da3238d083f7a408f7b1955c906bff28435
Pavel <Qw3st@mail.ru>
2022-12-17 22:29:50 +0300

    DZ_08

---
+++ DZ/DZ_08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Эта программа будет искать все четный числа от 1 до N");
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+int sum = 0;
+while (n<number) 
+{
+    if (n % 2 == 0)
+    sum = sum +n;
+    else
+    n = n + 1;
+n = n +1;    
+}
+Console.Write(sum);
