d1d3657f8e91f645896f836180e5bc4600c2a5aa
Pavel <Qw3st@mail.ru>
2022-12-17 20:50:44 +0300

    DZ_02

---
+++ DZ/DZ_02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > number1) max = number2;
+Console.Write($"Максимальное число равно {max}");
