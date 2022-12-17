97232346971c16eeed4bd97f99dbba5bd321f5c7
Pavel <Qw3st@mail.ru>
2022-12-17 11:38:08 +0300

    001

--- Seminar/Example_001/Program.cs
+++ Seminar/Example_001/Program.cs
@@ -1,6 +1,10 @@
 ﻿Console.WriteLine("Введите число:");// c помощью этого оператора мы вывродим на экран текст
+
 int number = Convert.ToInt32(Console.ReadLine());// c помощью этого оператора мы заставляем оператора ввести число
+
 int sqr = number * number; //ввели переменную
+
 // Console.Write("Квадрат числа "+ number + " равен: " +sqr);
+
 Console.Write($"Квадрат числа  {number} равен:  {sqr}"); // интерполяция
 
