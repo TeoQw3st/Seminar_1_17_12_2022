041dd7ce12a96c572d03b892a3a4845c0b9f8a11
Pavel <Qw3st@mail.ru>
2022-12-17 11:49:16 +0300

    002

---
+++ Seminar/Example_002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numFirst == numSecond*numSecond)
+    Console.Write($"Число {numFirst} является квадратом {numSecond}");
+else 
+    Console.Write($"Число {numFirst} не является квадратом {numSecond}");
