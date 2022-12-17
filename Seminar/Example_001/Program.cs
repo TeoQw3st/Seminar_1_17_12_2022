016fc2c47094912586abb03b45c0c9cc84d7a626
Pavel <Qw3st@mail.ru>
2022-12-17 11:31:54 +0300

    001

--- Seminar/Example_001/Program.cs
+++ Seminar/Example_001/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите число:");
+﻿Console.WriteLine("Введите число:");// c помощью этого оператора мы заставляем пользователя вводить число
 int number = Convert.ToInt32(Console.ReadLine());
 int sqr = number * number;
-Console.Write("Квадрат числа "+ number + " равен: " +sqr);
-
+// Console.Write("Квадрат числа "+ number + " равен: " +sqr);
+Console.Write($"Квадрат числа  {number} равен:  {sqr}"); // интерполяция
 
